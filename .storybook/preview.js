bbf5b4b0ba31ee76a8ed54992f2dcd5b5ad80789
omizha <ada.lovelace@kakao.com>
2023-04-01 16:33:30 +0900

    dep: Add tailwind&storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
+import '../src/tailwind.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'dark',
+    attributeName: 'data-mode',
+  }),
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  // Toggle dark-mode based on data-mode="dark"
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
